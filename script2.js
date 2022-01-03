fcc8d5134f5625a5b4d3739f2c5a4a5ed69e02e8
Lucy Lloyd <lucyeadam@gmail.com>
2022-01-03 16:14:08 +0000

    updated with DOM

--- script2.js
+++ script2.js
@@ -4,58 +4,113 @@
     let randomPlay = Math.floor(Math.random() * 3);
     
     if (randomPlay === 1) {
-        computer = "rock";
+        computerSelection = "rock";
         
     }
     else if (randomPlay === 2) {
-        computer = "scissors";
+        computerSelection = "scissors";
         
     } 
     else { 
-        computer = "paper";
+        computerSelection = "paper";
         }
 
-        console.log("computer is " + computer)
+        console.log("computer is " + computerSelection)
   
 }
 
+//make a heading to start play
+
+const container = document.querySelector('#container');
+
+const shoot = document.createElement('h1');
+shoot.classList.add('shoot');
+shoot.textContent = "Rock...Paper...Scissors...SHOOT!";
+shoot.style.color = 'blue';
+
+container.appendChild(shoot);
+
+const select = document.createElement('h3');
+select.classList.add('select');
+select.textContent = "you choose:";
+select.style.color = "grey";
+container.appendChild(select);
+
+const rockpic = document.createElement('img');
+select.classList.add('rockpic');
+rockpic.src = 'images/rock.png';
+rockpic.style.border = 'thick solid blue';
+rockpic.style.borderRadius = '50%'
+rockpic.height = "200";
+rockpic.style.margin = "10px";
+container.appendChild(rockpic);
+rockpic.onclick = () => selectChoice('rock');
+//make that onclick selection the player selection
+
+const scissorspic = document.createElement('img');
+select.classList.add('scissorspic');
+scissorspic.src = 'images/scissors.png';
+scissorspic.style.borderRadius = '50%'
+scissorspic.height = "200";
+scissorspic.style.margin = "10px";
+scissorspic.style.border = 'thick solid blue';
+container.appendChild(scissorspic);
+scissorspic.onclick = () => selectChoice('scissors');
+
+const paperpic = document.createElement('img');
+select.classList.add('paperpic');
+paperpic.src = "images/paper.png";
+paperpic.style.borderRadius = '50%'
+paperpic.height = "200";
+paperpic.style.margin = "10px";
+paperpic.style.border = 'thick solid blue';
+container.appendChild(paperpic);
+paperpic.onclick = () => selectChoice("paper")
+//paperpic.addEventListener = ("click", selectChoice('paper'));
 
 
+function selectChoice(play) {
+    yourSelection.textContent = `You chose ${play}`;
+    console.log("you chose " + play);
+           computerPlay();
+           singleGame(play, computerSelection);
+           
+    }
 
-
-//* make a single round of game, with computer and player parameters
-function player() {
-    prompting = prompt("Rock, Paper, Scissors- SHOOT!");
-    play = prompting.toLowerCase();
-    console.log("You are " + play);
-
-}
 
 
 function singleGame( playerSelection, computerSelection){
-
+    compSelection.textContent = `Computer chose ${computerSelection}`;
     
 
     if (playerSelection == "paper" && computerSelection == "rock" 
     || playerSelection == "rock" && computerSelection == "scissors" 
     || playerSelection == "scissors" && computerSelection == "paper" ){
         console.log("You win, " + playerSelection + " beats " + computerSelection);
- tallyPlayer += 1;
+
+        winnerDeclaration.textContent = `You win, ${playerSelection} beats ${computerSelection}!`
+tallyPlayer += 1;
  tallyGame += 1;
 
 }
 
 else if (playerSelection == computerSelection) {
-    console.log("Oops! It's a match!")
+    winnerDeclaration.textContent = `Oops! It's a match!`
     tallyGame += 1;
 }
       
-    else { console.log("You lose, " + computerSelection + " beats " + playerSelection);  
+    else { 
+        winnerDeclaration.textContent = `You lose, ${computerSelection} beats ${playerSelection}!` 
+        console.log("You lose, " + computerSelection + " beats " + playerSelection);  
     tallyComp += 1;
     tallyGame += 1;
 }
-    
+console.log(tallyComp);
+    runningScore.textContent = `Score : you ${tallyPlayer} : computer ${tallyComp}`;
+    winning();
 }
+
+
 
 
 
@@ -64,29 +119,48 @@ else if (playerSelection == computerSelection) {
 let tallyPlayer = 0;
     let tallyComp = 0;
     let tallyGame = 1;
-    
-function game() {
-    
-    while (tallyGame <= 5) {
-        computerPlay();
-        player();
-      
-singleGame(play, computer);
-console.log(tallyPlayer + " vs " + tallyComp);
+ 
 
-    }
-    
-winning();
-};
 
-game()
 
 function winning(){
-    if (tallyPlayer > tallyComp) {
+    if (tallyPlayer == 5) {
         alert("You win! Well done")
     }
-    else if (tallyComp = tallyPlayer) {
-        alert("It's a tie")
+    else if (tallyComp == 5) {
+        alert("Computer wins!")
     }
-    else alert("Computer wins!")
+   
 }
+
+
+//updating the DOM with the game status
+const gameStatus = document.createElement('div');
+gameStatus.classList.add('gameStatus');
+container.appendChild(gameStatus);
+
+
+const yourSelection = document.createElement('h3');
+yourSelection.classList.add('yourSelection');
+yourSelection.textContent = `Your selection please`;
+yourSelection.style.color = "lilac";
+gameStatus.appendChild(yourSelection);
+
+const compSelection = document.createElement('h3');
+compSelection.classList.add('compSelection');
+
+compSelection.style.color = "grey";
+gameStatus.appendChild(compSelection);
+
+const winnerDeclaration = document.createElement('h1');
+winnerDeclaration.classList.add('winnerDeclaration');
+
+winnerDeclaration.style.color = "pink";
+gameStatus.appendChild(winnerDeclaration);
+
+const runningScore = document.createElement('h2');
+runningScore.classList.add('runningScore');
+
+runningScore.style.color = "red";
+gameStatus.appendChild(runningScore);
+
